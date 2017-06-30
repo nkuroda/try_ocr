a3344b8487899d7cfab927f2608c4c49114f2f0f
Kuroda.Naoki <kuroda.naoki@moneyforward.co.jp>
2017-06-30 14:52:45 +0900

    🆕  Add Receipt#words method

--- lib/try_ocr/receipt.rb
+++ lib/try_ocr/receipt.rb
@@ -8,7 +8,7 @@ module TryOcr
 
     # OCRで読み取った全文を返す
     def full_text
-      image.text.words.map(&:text).join.strip
+      words.map(&:text).join.strip
     end
 
     # レシートの日付を返す。認識できなければnilを返す
@@ -21,6 +21,13 @@ module TryOcr
 
     # レシートの電話番号を返す。認識できなければnilを返す
     def tel
+    end
+
+    private
+
+    # 単語の配列
+    def words
+      @words = image.text&.words
     end
   end
 end
