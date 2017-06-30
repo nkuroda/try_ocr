6b86e0bc0082d9dc547cecd30dd31b3ebdc39b27
Kuroda.Naoki <kuroda.naoki@moneyforward.co.jp>
2017-06-30 14:44:02 +0900

    🆕  Add second test image file

--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -11,5 +11,11 @@ describe TryOcr::Receipt do
 
       it { is_expected.to eq '日本語' }
     end
+
+    context 'when second image' do
+      let(:source) { "#{RSPEC_ROOT}/fixtures/files/second_image.png" }
+
+      it { is_expected.to eq '領収書毎度ありがとうございます様[証紙切手引受]第一種定形界で規格内)23.00¥120@120小計第一種定形外(規格内)¥12067.0g¥140@140特殊取扱¥310¥310Y450(内訳)簡易書留小計郵便物引受合計通数課税(内消費税等非課税言2通Y570¥42)Y0合計¥570¥1,000お預り金額おつリ¥430' }
+    end
   end
 end
